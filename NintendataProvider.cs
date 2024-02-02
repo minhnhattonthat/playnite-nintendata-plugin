460d3fed1d9d805ec3dca3af698347f05a3b0149
Nathan Ton <minhnhattonthat@gmail.com>
2024-02-03 00:16:10 +0700

    Init commit

---
+++ NintendataProvider.cs
@@ -0,0 +1,270 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Nintendata
+{
+    public class NintendataProvider : OnDemandMetadataProvider
+    {
+        private readonly MetadataRequestOptions options;
+        private readonly Nintendata plugin;
+        private readonly IPlayniteAPI playniteApi;
+
+        private NintendoEshopClient client;
+        private NintendoGame game;
+        private static readonly ILogger logger = LogManager.GetLogger();
+        private List<MetadataField> availableFields;
+        public override List<MetadataField> AvailableFields
+        {
+            get
+            {
+                if (availableFields == null)
+                {
+                    availableFields = GetAvailableFields();
+                }
+
+                return availableFields;
+            }
+        }
+
+        public NintendataProvider(MetadataRequestOptions options, Nintendata plugin)
+        {
+            this.options = options;
+            this.plugin = plugin;
+            this.playniteApi = plugin.PlayniteApi;
+            this.client = new NintendoEshopClient(options);
+        }
+
+        private List<MetadataField> GetAvailableFields()
+        {
+            if (this.game == null)
+            {
+                GetNintendoGameMetadata();
+            }
+
+            var fields = new List<MetadataField> { MetadataField.Links };
+
+            fields.Add(MetadataField.Name);
+
+            if (!string.IsNullOrEmpty(game.FullDescription))
+            {
+                fields.Add(MetadataField.Description);
+            }
+
+            if (game.ReleaseDate != null && game.ReleaseDate?.Year != null)
+            {
+                fields.Add(MetadataField.ReleaseDate);
+            }
+
+            if (game.Developers.Count > 0)
+            {
+                fields.Add(MetadataField.Developers);
+            }
+
+            if (game.Publishers.Count > 0)
+            {
+                fields.Add(MetadataField.Publishers);
+            }
+
+            if (game.Genres.Count > 0)
+            {
+                fields.Add(MetadataField.Genres);
+            }
+
+            if (game.Series.Count > 0)
+            {
+                fields.Add(MetadataField.Series);
+            }
+
+            if (game.Links.Count > 0)
+            {
+                fields.Add(MetadataField.Links);
+            }
+
+            if (game.Image != null && !string.IsNullOrEmpty(game.Image.Path))
+            {
+                fields.Add(MetadataField.CoverImage);
+            }
+
+            if (game.AgeRatings.Count > 0)
+            {
+                fields.Add(MetadataField.AgeRating);
+            }
+
+            return fields;
+        }
+
+        private void GetNintendoGameMetadata()
+        {
+            if (this.game != null)
+            {
+                return;
+            }
+
+            if (!options.IsBackgroundDownload)
+            {
+                logger.Debug("not background");
+                var item = plugin.PlayniteApi.Dialogs.ChooseItemWithSearch(null, (a) =>
+                {
+                    return client.SearchGames(NormalizeSearchString(a));
+                }, options.GameData.Name);
+
+                if (item != null)
+                {
+                    this.game = (NintendoGame)item;
+                }
+                else
+                {
+                    this.game = new NintendoGame();
+                    logger.Warn($"Cancelled search");
+                }
+            }
+            else
+            {
+                try
+                {
+                    List<GenericItemOption> results = client.SearchGames(NormalizeSearchString(options.GameData.Name));
+
+                    switch (results.Count)
+                    {
+                        case 0:
+                            this.game = new NintendoGame();
+                            break;
+                        case 1:
+                            this.game = (NintendoGame)results.First();
+                            break;
+                        default:
+                            var words = SplitStringToWords(NormalizeSearchString(options.GameData.Name));
+                            var nameFullyMatchedResult = results.FirstOrDefault(game => words.All(w => game.Name.ToLower().Contains(w)));
+                            if (nameFullyMatchedResult != null)
+                            {
+                                this.game = (NintendoGame)nameFullyMatchedResult;
+                            }
+                            else
+                            {
+                                this.game = new NintendoGame();
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e, "Failed to get Nintendo game metadata.");
+                    this.game = new NintendoGame();
+                }
+            }
+
+        }
+        private string NormalizeSearchString(string search)
+        {
+            return new Regex(@"\[.*\]").Replace(search, "").Replace("-", " ").Replace(":", "").ToLower();
+        }
+
+        private string[] SplitStringToWords(string normalizedString)
+        {
+            return new Regex(@"\W").Split(normalizedString);
+        }
+
+        public override string GetName(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.Name))
+            {
+                return this.game.Title;
+            }
+
+            return base.GetName(args);
+        }
+
+        public override string GetDescription(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.Description))
+            {
+                return this.game.FullDescription;
+            }
+
+            return base.GetDescription(args);
+        }
+
+        public override ReleaseDate? GetReleaseDate(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.ReleaseDate))
+            {
+                return this.game.ReleaseDate;
+            }
+
+            return base.GetReleaseDate(args);
+        }
+
+        public override IEnumerable<MetadataProperty> GetPublishers(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.Publishers))
+            {
+                return this.game.Publishers;
+            }
+
+            return base.GetPublishers(args);
+        }
+
+        public override IEnumerable<MetadataProperty> GetDevelopers(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.Developers))
+            {
+                return this.game.Developers;
+            }
+
+            return base.GetDevelopers(args);
+        }
+
+        public override IEnumerable<MetadataProperty> GetGenres(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.Genres))
+            {
+                return this.game.Genres;
+            }
+
+            return base.GetGenres(args);
+        }
+
+        public override IEnumerable<MetadataProperty> GetSeries(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.Series))
+            {
+                return this.game.Series;
+            }
+
+            return base.GetSeries(args);
+        }
+
+        public override IEnumerable<MetadataProperty> GetAgeRatings(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.AgeRating))
+            {
+                return this.game.AgeRatings;
+            }
+            return base.GetAgeRatings(args);
+        }
+
+        public override IEnumerable<Link> GetLinks(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.Links))
+            {
+                return this.game.Links;
+            }
+
+            return base.GetLinks(args);
+        }
+
+        public override MetadataFile GetCoverImage(GetMetadataFieldArgs args)
+        {
+            if (AvailableFields.Contains(MetadataField.CoverImage))
+            {
+                return this.game.Image;
+            }
+            return base.GetCoverImage(args);
+        }
+    }
+}
---
+++ NintendoGame.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json.Linq;
+using Playnite.SDK;
+using Playnite.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nintendata
+{
+    public class NintendoGame : GenericItemOption
+    {
+        private readonly ILogger logger = LogManager.GetLogger();
+
+        private readonly string title;
+        public string Title { get { return title; } }
+
+        private readonly string fullDescription;
+        public string FullDescription { get { return fullDescription; } }
+
+        private ReleaseDate? releaseDate;
+        public ReleaseDate? ReleaseDate { get { return releaseDate; } }
+
+        private readonly List<MetadataProperty> developers;
+        public List<MetadataProperty> Developers { get { return developers; } }
+
+        private readonly List<MetadataProperty> publishers;
+        public List<MetadataProperty> Publishers { get { return publishers; } }
+
+        private readonly List<MetadataProperty> genres;
+        public List<MetadataProperty> Genres { get { return genres; } }
+
+        private readonly List<MetadataProperty> series;
+        public List<MetadataProperty> Series { get { return series; } }
+
+        private readonly List<MetadataProperty> ageRatings;
+        public List<MetadataProperty> AgeRatings { get { return ageRatings; } }
+
+        private readonly List<Link> links;
+        public List<Link> Links { get { return links; } }
+
+        private readonly MetadataFile image;
+        public MetadataFile Image { get { return image; } }
+
+        public Game LibraryGame;
+
+        public NintendoGame()
+        {
+            this.developers = new List<MetadataProperty>();
+            this.publishers = new List<MetadataProperty>();
+            this.genres = new List<MetadataProperty>();
+            this.series = new List<MetadataProperty>();
+            this.ageRatings = new List<MetadataProperty>();
+            this.links = new List<Link>();
+        }
+
+        public NintendoGame(JObject data) : this()
+        {
+            logger.Info($"JOBject {data}");
+
+            this.title = NormalizeTitle((string)data["title"]);
+
+            this.fullDescription = (string)data["description"];
+
+            var releaseDateTime = (DateTime)data["releaseDate"];
+            this.releaseDate = new ReleaseDate(releaseDateTime);
+
+            var dev = (string)data["softwareDeveloper"];
+
+            if (!string.IsNullOrEmpty(dev))
+            {
+                this.developers.Add(new MetadataNameProperty(dev));
+            }
+
+            this.publishers.Add(new MetadataNameProperty((string)data["softwarePublisher"]));
+
+            foreach (dynamic genre in data["genres"])
+            {
+                this.genres.Add(new MetadataNameProperty((string)genre));
+            }
+
+            foreach (dynamic franchise in data["franchises"])
+            {
+                this.series.Add(new MetadataNameProperty((string)franchise));
+            }
+
+            var esrbRating = $"ESRB {(string)data["esrbRating"]}";
+            this.ageRatings.Add(new MetadataNameProperty(esrbRating));
+
+            this.links.Add(new Link("My Nintendo Store", $"https://www.nintendo.com{(string)data["url"]}"));
+
+            var imageUrl = $"https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.5/c_scale,w_700/{(string)data["productImage"]}";
+            this.image = new MetadataFile(imageUrl);
+
+            this.Name = this.title;
+            this.Description = $"{releaseDateTime.Year}-{releaseDateTime.Month}-{releaseDateTime.Day} | {publishers.First()}";
+        }
+
+        private string NormalizeTitle(string title)
+        {
+            return title.Replace("™", "");
+        }
+
+    }
+}
