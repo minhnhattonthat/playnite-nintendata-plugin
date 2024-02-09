84c23702597205ca5a5ff2cc546157c36483cf43
Nathan Ton <minhnhattonthat@gmail.com>
2024-02-09 23:10:30 +0700

    Support get description from details page

---
+++ Client/UKNintendoClient.cs
@@ -0,0 +1,91 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json.Linq;
+using Playnite.SDK;
+using Playnite.SDK.Plugins;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NintendoMetadata.Client
+{
+    public class UKNintendoClient : NintendoClient
+    {
+
+        protected override string BaseUrl => "http://search.nintendo-europe.com/en/select";
+
+        public UKNintendoClient(MetadataRequestOptions options, NintendoMetadataSettings settings) : base(options, settings)
+        {
+        }
+
+        public override List<NintendoGame> SearchGames(string normalizedSearchName)
+        {
+            List<NintendoGame> results = new List<NintendoGame>();
+
+            var request = new RestRequest("/");
+            var parameters = new
+            {
+                q = normalizedSearchName,
+                fq = "type:GAME AND system_type:nintendoswitch*",
+                sort = "score desc, date_from desc",
+                start = 0,
+                rows = 24,
+                wt = "json",
+            };
+            request.AddObject(parameters);
+
+            try
+            {
+                JObject response = ExecuteRequest(request);
+
+                if (!response.TryGetValue("response", out JToken resultsData))
+                {
+                    logger.Error($"Encountered API error: [{response["error"]["code"]}] {response["error"]["msg"]}");
+                    return results;
+                }
+
+                var docs = resultsData["docs"];
+                logger.Debug($"SearchGames {resultsData["numFound"]} results for {normalizedSearchName}");
+
+                foreach (dynamic game in docs)
+                {
+                    NintendoGame g = NintendoGame.ParseUkGame(game);
+                    results.Add(g);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Error performing search");
+            }
+
+            return results.OrderBy(game => NameStringCompare(normalizedSearchName, game.Name)).ToList();
+        }
+
+        public override NintendoGame GetGameDetails(NintendoGame game)
+        {
+            var link = game.Links.FirstOrDefault(l => l.Name == "My Nintendo Store");
+
+            if (link == null)
+            {
+                return game;
+            }
+
+            var web = new HtmlWeb()
+            {
+                OverrideEncoding = Encoding.UTF8
+            };
+            var doc = web.Load(link.Url);
+            var descriptionNode = doc.DocumentNode.SelectSingleNode(@"//section[@id='Overview']");
+            if (descriptionNode != null)
+            {
+                game.FullDescription = Regex.Replace(descriptionNode.InnerHtml, @"\s{2,}", "");
+                logger.Info(game.FullDescription);
+            }
+
+            return game;
+        }
+    }
+}
---
+++ Client/USANintentdoClient.cs
@@ -0,0 +1,110 @@
+﻿using Playnite.SDK.Plugins;
+using Playnite.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using HtmlAgilityPack;
+using Playnite.SDK.Models;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls;
+
+namespace NintendoMetadata.Client
+{
+    public class USANintendoClient : NintendoClient
+    {
+        private const string UsAlgoliaId = "U3B6GR4UA3";
+        private const string UsAlgoliaKey = "a29c6927638bfd8cee23993e51e721c9";
+
+        protected override string BaseUrl => $"https://{UsAlgoliaId}-2.algolia.net/1/indexes/*/queries";
+
+        public USANintendoClient(MetadataRequestOptions options, NintendoMetadataSettings settings) : base(options, settings)
+        {
+        }
+
+        protected override void InitRestClient()
+        {
+            base.InitRestClient();
+            restClient
+                    .AddDefaultHeader("Content-Type", "application/json")
+                    .AddDefaultHeader("Accept", "*/*")
+                    .AddDefaultHeader("X-Algolia-API-Key", UsAlgoliaKey)
+                    .AddDefaultHeader("X-Algolia-Application-Id", UsAlgoliaId);
+        }
+
+        public override List<NintendoGame> SearchGames(string normalizedSearchName)
+        {
+            List<NintendoGame> results = new List<NintendoGame>();
+
+            var request = new RestRequest("/", Method.Post);
+            var body = $@"{{""requests"": [{{""indexName"": ""store_game_en_us"",""query"": ""{normalizedSearchName}"",""params"": ""hitsPerPage=10""}}]}}";
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(body);
+
+            try
+            {
+                JObject response = ExecuteRequest(request);
+
+                if (!response.TryGetValue("results", out JToken resultsData))
+                {
+                    logger.Error($"Encountered API error: [{response["status"]}] {response["message"]}");
+                    return results;
+                }
+
+                var result = resultsData[0];
+                logger.Debug($"SearchGames {result["nbHits"]} results for {normalizedSearchName}");
+
+                foreach (dynamic game in result["hits"])
+                {
+                    NintendoGame g = NintendoGame.ParseUsGame(game);
+                    results.Add(g);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Error performing search");
+            }
+
+            return results.OrderBy(game => NameStringCompare(normalizedSearchName, game.Name)).ToList();
+        }
+
+        public override NintendoGame GetGameDetails(NintendoGame game)
+        {
+            var link = game.Links.FirstOrDefault(l => l.Name == "My Nintendo Store");
+
+            if (link == null)
+            {
+                return game;
+            }
+
+            var web = new HtmlWeb();
+            var doc = web.Load(link.Url);
+            var descriptionNode = doc.DocumentNode.SelectSingleNode(@"//div[@class='ProductDetailstyles__Grid-sc-4l5ex7-4 hKLOzA']//p");
+            if (descriptionNode != null)
+            {
+                game.FullDescription = descriptionNode.InnerHtml;
+                logger.Info(game.FullDescription);
+            }
+            else
+            {
+                var nextData = doc.DocumentNode.SelectSingleNode(@"//script[@id='__NEXT_DATA__']");
+                if (nextData != null)
+                {
+                    var matches = Regex.Matches(nextData.InnerText, @"""description"":""(.*?)""");
+                    if (matches.Count > 1 && matches[matches.Count - 1].Success)
+                    {
+                        string fullDescription = Regex.Unescape(matches[matches.Count - 1].Groups[1].Value);
+                        fullDescription = fullDescription.Substring(3, fullDescription.Length - 7);
+                        game.FullDescription = fullDescription;
+                    }
+                    //var json = JObject.Parse(nextData.InnerText);
+                }
+            }
+
+            return game;
+        }
+    }
+}
